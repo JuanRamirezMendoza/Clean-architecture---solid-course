34b07998a20ed70b3491655859cffce4676c62c0
JuanRamirezMendoza <ramirezsebastian72@gmail.com>
2022-02-14 16:52:08 -0500

    add module core and Note model

--- app/src/main/java/com/study/cleanarchitecturesolidprinciplesforandroidinkotlin/presentation/MainActivity.kt
+++ app/src/main/java/com/study/cleanarchitecturesolidprinciplesforandroidinkotlin/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.study.cleanarchitecturesolidprinciplesforandroidinkotlin
+package com.study.cleanarchitecturesolidprinciplesforandroidinkotlin.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.study.cleanarchitecturesolidprinciplesforandroidinkotlin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
