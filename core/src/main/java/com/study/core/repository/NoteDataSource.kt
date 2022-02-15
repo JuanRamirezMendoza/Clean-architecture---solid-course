bdd4a6e374d452d1771e5088f5000ae3f9f9342c
DESKTOP-1EP2T3R\Sebastian Ramirez <sebastianramirez@wposs.com>
2022-02-14 20:03:29 -0500

    Implementation repository

---
+++ core/src/main/java/com/study/core/repository/NoteDataSource.kt
@@ -0,0 +1,13 @@
+package com.study.core.repository
+
+import com.study.core.data.Note
+
+interface NoteDataSource {
+    suspend fun add(note: Note)
+
+    suspend fun get(id: Long): Note?
+
+    suspend fun getAll(): List<Note>
+
+    suspend fun remove(note:Note)
+}
